48e110d9b984d2c42fb4cec9a92dc46e273ec7f8
optroot <optroot@gmail.com>
2019-01-22 23:42:49 -0500

    Update .user.js
    Blind find/replace genBTC to optroot so can install and run

--- .user.js
+++ .user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         AutoTrimps-genBTC
 // @version      2.1.6.9-genbtc-3-23-2018
-// @namespace    https://genbtc.github.io/AutoTrimps
-// @updateURL    https://genbtc.github.io/AutoTrimps/.user.js
+// @namespace    https://optroot.github.io/AutoTrimps
+// @updateURL    https://optroot.github.io/AutoTrimps/.user.js
 // @description  Automate all the trimps!
 // @author       zininzinin, spindrjr, Ishkaru, genBTC
 // @include      *trimps.github.io*
 // @include      *kongregate.com/games/GreenSatellite/trimps
-// @connect      *genbtc.github.io/AutoTrimps*
+// @connect      *optroot.github.io/AutoTrimps*
 // @connect      *trimps.github.io*
 // @connect      self
 // @grant        none
@@ -16,7 +16,7 @@
 var script = document.createElement('script');
 script.id = 'AutoTrimps-script';
 //This can be edited to point to your own Github Repository URL.
-script.src = 'https://genBTC.github.io/AutoTrimps/AutoTrimps2.js';
+script.src = 'https://optroot.github.io/AutoTrimps/AutoTrimps2.js';
 //script.setAttribute('crossorigin',"use-credentials");
 script.setAttribute('crossorigin',"anonymous");
 document.head.appendChild(script);
